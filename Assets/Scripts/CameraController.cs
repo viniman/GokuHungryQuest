201bc16e864b085c95fff0d244fdcd904455eda0
FelipeTerrana <felipetc_7@hotmail.com>
2018-12-08 22:14:37 -0200

    Gerar inimigos aleatoriamente

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player;
+	private float previousPlayerY;
+	private float playerOffsetY;
+
+	// Use this for initialization
+	void Start ()
+	{
+		previousPlayerY = player.position.y;
+		playerOffsetY = -previousPlayerY;
+		transform.position = new Vector3(0, previousPlayerY + playerOffsetY, -10);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(player.position.y > previousPlayerY) // Câmera só anda pra frente
+		{
+			transform.position = new Vector3(0, player.position.y + playerOffsetY, -10);
+			previousPlayerY = player.position.y;
+		}
+	}
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+	public GameObject easyEnemy;
+	public GameObject mediumEnemy;
+	public GameObject hardEnemy;
+
+	/// Indica quantos inimigos (mais ou menos) serão renderizados em cada bloco de dimensão 10x10
+	public float enemiesPer10x10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Renderer renderer = GetComponent<Renderer>();
+		float levelArea = renderer.bounds.size.x * renderer.bounds.size.y;
+
+		/// Número total de inimigos a serem criados na fase, levelArea / 100 representa quantos
+		/// blocos de 10x10 cabem na fase
+		int enemiesToRender = Mathf.FloorToInt( (levelArea / 100) * enemiesPer10x10 );
+
+		float maxX = renderer.bounds.max.x - 2;
+		float maxY = renderer.bounds.max.y - 2;
+		float minX = renderer.bounds.min.x + 2;
+		float minY = renderer.bounds.min.y + 2;
+
+		for(int i=0; i < enemiesToRender; i++)
+		{
+			GameObject enemy;
+
+			if( Random.value < 0.75 )
+				enemy = Instantiate(easyEnemy);
+			else if( Random.value < 0.75 )
+				enemy = Instantiate(mediumEnemy);
+			else
+				enemy = Instantiate(hardEnemy);
+
+			enemy.transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
