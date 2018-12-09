9dc966ea6c96bee803a4108f809a443656643230
Warley Almeida <almeida.warley@outlook.com>
2018-12-08 23:20:00 -0200

    Adição das animações do Goku

---
+++ Assets/MediumEnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumEnemyController : MonoBehaviour {
+
+    private int direction;
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        float random = Random.Range(0, 10);
+        if (random < 5) direction = 1; else direction = -1;
+        speed = 5.0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 10 || transform.position.x < -10)
+            direction = direction * -1;
+        if (direction == 1)
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        else
+            transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        transform.Translate(direction * speed * Time.deltaTime * transform.right);
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = direction * -1;
+    }
+}
--- Assets/Scripts/GokuController.cs
+++ Assets/Scripts/GokuController.cs
@@ -7,13 +7,22 @@ public class GokuController : MonoBehaviour {
     public float speed;
     private int coinCounter;
 
+    private int direction; // 1 while going right, -1 while going left, 0 while going up or down
+    private int facing; // 1 while going up, -1 while going down, 0 while going right or left
+    private bool walking;
+
     private GameObject projectilePreFab;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
         coinCounter = 0;
+        walking = false;
+        direction = 0;
+        facing = 1;
 
         projectilePreFab = Resources.Load("Projectile") as GameObject;
+        animator = GetComponent<Animator>();
 		
 	}
 	
@@ -21,26 +30,49 @@ public class GokuController : MonoBehaviour {
 	void Update () {
 
         handleInput();
+
+        // changes angle according to current direction
+        if (direction == 1) transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f); 
+        if (direction == -1) transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        // changes animation according to current move
+        walking = (direction != 0 || facing != 0);
+        animator.SetBool("walking", walking);
+        animator.SetInteger("facing", facing);
+
+
 	}
 
     // Handles input from keyboard
     void handleInput()
     {
+        direction = 0;
+        facing = 0;
         if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Translate(transform.right * speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.LeftArrow))
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.Translate(transform.right * -1 * speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.UpArrow))
+            direction = -1;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
             transform.Translate(transform.up * speed * Time.deltaTime);
+            facing = 1;
+        }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             /// Menor altura no campo de visão da câmera
             float lowerBound = Camera.main.transform.position.y - Camera.main.orthographicSize;
 
             /// Só move o Goku pra baixo dentro da visão da câmera
-            if(transform.position.y > lowerBound + 1)
+            if(transform.position.y > lowerBound + 1){
                 transform.Translate(transform.up * -1 * speed * Time.deltaTime);
+                facing = -1;
+            }
         }
             
         if (Input.GetKeyDown(KeyCode.Space))
@@ -61,6 +93,7 @@ public class GokuController : MonoBehaviour {
 
     void die()
     {
+        Destroy(gameObject);
 
     }
 
